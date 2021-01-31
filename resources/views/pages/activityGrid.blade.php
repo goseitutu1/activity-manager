e185c248edf662eb4d335ccc41faafae202645bc
goseitutu1 <oseitutugideon1@gmail.com>
2021-01-31 04:26:22 +0100

    select date form

--- resources/views/pages/activityGrid.blade.php
+++ resources/views/pages/activityGrid.blade.php
@@ -1,6 +1,26 @@
 @extends('layouts.default')
 @section('content')
 <div class="row">
+    <div class="container">
+        <form method="POST" action="/date" enctype="multipart/form-data" id="search-form" role="form" style="margin-top: 3%; margin-bottom: 1%;">
+            {{ csrf_field() }}
+            <div class="form-row">
+                <div class="col-md-2"></div>
+                    <div class="form-group col-md-3" id="query_form">
+                            <span class="h5 mb-2">Start Date</span>
+                            <input class="form-control"  type="date" id="start" name="start_date" required>
+                    </div>
+                    <div class="form-group col-md-3">
+                        <span class="h5 mb-2">End Date</span>
+                        <input class="form-control" type="date" id="end" name="end_date" required>
+                    </div>
+                    <div class="col-md-3" style="margin-top:2.5%;">
+                        <input class="btn bg-secondary" id="s_button" type="submit" value="Filter Action">
+                    </div>
+            </div>
+        </form>
+    </div>
+    
     <div class="col-10">
         <div class="card">
             <div class="card-header">
@@ -48,7 +68,7 @@
                     </tbody>
                     @endforeach
                 </table>
-                {{$activities->links()}}
+                {{-- {{$activities->links()}} --}}
                 @else
                    <h1>No posts</h1>
                 @endif
